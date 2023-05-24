ba663966ea07315a942948d12bc90e3d617cafb7
YONGJUN CHO <as2686@naver.com>
2023-05-24 18:46:31 +0900

    Create Project

---
+++ src/main/java/com/tave/connectX/ConnectXApplication.java
@@ -0,0 +1,13 @@
+package com.tave.connectX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tave/connectX/ConnectXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tave.connectX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
