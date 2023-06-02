2447b1b74a4431a9164940dddc196c12239afd2f
JUN <as2686@naver.com>
2023-06-02 14:30:51 +0900

    [Feat] 유저 식별자, 프로필 이미지 (썸네일), 닉네임 받는 기능 구현

--- src/main/java/com/tave/connectX/controller/LoginController.java
+++ src/main/java/com/tave/connectX/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.tave.connectX.controller;
 
 import com.tave.connectX.dto.OAuthToken;
+import com.tave.connectX.dto.OAuthUserInfo;
 import com.tave.connectX.service.OAuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class LoginController {
@@ -19,10 +24,9 @@ public class LoginController {
     public ResponseEntity responseEntity(@PathVariable String code) {
 
         OAuthToken kakaoAccessToken = oAuthService.getKakaoAccessToken(code);
+        OAuthUserInfo userInfo = oAuthService.getUserInfo(kakaoAccessToken);
 
-
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(userInfo.getOAuthId() + userInfo.getNickName() +", " + userInfo.getProfileImage());
     }
 
 }
--- src/main/java/com/tave/connectX/service/OAuthService.java
+++ src/main/java/com/tave/connectX/service/OAuthService.java
@@ -1,6 +1,7 @@
 package com.tave.connectX.service;
 
 import com.tave.connectX.dto.OAuthToken;
+import com.tave.connectX.dto.OAuthUserInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,23 @@ import java.util.Map;
 @Service
 public class OAuthService {
     static final String URI_KAKAO = "https://kauth.kakao.com";
+    static final String URI_KAKAO_API = "https://kapi.kakao.com";
+
     @Value(value = "${oauth.secret.key}")
     private String RESTAPIKEY = "TAVE11TH";
     @Value(value = "${oauth.redirect.uri}")
     private String REDIRECTURI = "TAVE11TH";
 
+    // 프론트에서 인가 코드를 받아 토큰 받아오는 메서드
     public OAuthToken getKakaoAccessToken(String authorizationCode) {
 
         try {
 
+            /**
+             * [KAKAO REST API] 요청할 정보 입력 - URI, Headers, Body( Parameter )
+             */
+            
+            // set uri
             String requestUrl = URI_KAKAO + "/oauth/token";
 
             // set headers
@@ -48,6 +57,30 @@ public class OAuthService {
         } catch (RuntimeException e) {
             return null;
         }
+    }
+
+    // 토큰으로 사용자 정보 받아오는 메서드
+    public OAuthUserInfo getUserInfo(OAuthToken oAuthToken) {
+
+        /**
+         * [KAKAO REST API] 요청할 정보 입력 - URI, Headers
+         */
+
+        String requestUri = URI_KAKAO_API + "/v2/user/me";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.set("Authorization", "Bearer " + oAuthToken.getAccessToken());
+
+        HttpEntity httpEntity = new HttpEntity(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String, Object> attributes = restTemplate.exchange(requestUri, HttpMethod.GET, httpEntity, Map.class).getBody();
+
+        OAuthUserInfo oAuthUserInfo = OAuthUserInfo.ofKakao(attributes);
+
+        return oAuthUserInfo;
     }
 
 
