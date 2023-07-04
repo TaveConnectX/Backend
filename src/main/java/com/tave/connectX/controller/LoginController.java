412d5b8eb1f23e89e9e273d0f6aec82b0b79aa85
asas6978 <asas6978@naver.com>
2023-07-04 17:52:32 +0900

    refactor: 메서드 설명 추가

--- src/main/java/com/tave/connectX/controller/LoginController.java
+++ src/main/java/com/tave/connectX/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.tave.connectX.dto.OAuthToken;
 import com.tave.connectX.dto.OAuthUserInfo;
 import com.tave.connectX.dto.User;
 import com.tave.connectX.service.OAuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ public class LoginController {
 
     private final OAuthService oAuthService;
 
+    @Operation(summary = "JWT 토큰 얻기", description = "인가코드를 이용해 JWT 토큰을 발급받는 메서드 입니다.")
     @GetMapping("/login/{code}")
     public ResponseEntity responseEntity(@PathVariable String code, HttpServletResponse response) {
         try {
