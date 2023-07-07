b0e65a0dc4b3aa1b9e6ab0c225bb8f10a8a6604f
asas6978 <asas6978@naver.com>
2023-07-07 23:34:34 +0900

    fix: swagger cors 문제 해결

--- src/main/java/com/tave/connectX/config/SwaggerConfig.java
+++ src/main/java/com/tave/connectX/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -15,6 +16,9 @@ import org.springframework.context.annotation.Configuration;
                 version = "v1"),
         security = {
                 @SecurityRequirement(name = "Authorization")
+        },
+        servers = {
+                @Server(url = "/")
         }
 )
 @SecuritySchemes({
